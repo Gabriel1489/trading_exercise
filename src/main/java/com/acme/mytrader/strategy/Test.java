d63fc626db6092414e1c3e477c64972dbc2154e8
Gabriel Selvakumar <85058847+Gabriel1489@users.noreply.github.com>
2021-10-24 10:52:49 +0100

    Delete Test.java

--- src/main/java/com/acme/mytrader/strategy/Test.java
+++
@@ -1,9 +0,0 @@
-package com.acme.mytrader.strategy;
-
-public class Test {
-	public static void main (String[] args) {
-		TradingStrategy td = new TradingStrategy();
-		td.priceUpdate("IBM", 101);
-	}
-
-}
